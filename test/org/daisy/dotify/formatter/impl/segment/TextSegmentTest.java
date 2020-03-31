97f6c73b3af4851d56cf5b9d0eb8f71ceeb0f079
Carl Wålinder <carl@textalk.se>
2020-03-31 09:21:17 +0200

    Code style changes

--- test/org/daisy/dotify/formatter/impl/segment/TextSegmentTest.java
+++ test/org/daisy/dotify/formatter/impl/segment/TextSegmentTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
+/**
+ *  Test class for TextSegment.
+ */
 public class TextSegmentTest {
     @Test
    public void testGetLocaleReturnEmptyOptionalIfLocaleIsNull() {
@@ -15,7 +19,7 @@ public class TextSegmentTest {
         TextProperties tp = builder.build();
         String chars = "";
         TextSegment ts = new TextSegment(chars, tp, false);
-        Optional <String> locale = ts.getLocale();
+        Optional<String> locale = ts.getLocale();
         assertEquals(Optional.empty(), locale);
     }
 
@@ -26,9 +30,9 @@ public class TextSegmentTest {
         TextProperties tp = builder.build();
         String chars = "";
         TextSegment ts = new TextSegment(chars, tp, false);
-        Optional <String> locale = ts.getLocale();
-        String locale_swe = "sv_SE";
-        assertNotEquals(locale, locale_swe);
-        assertNotEquals(null, locale_swe);
+        Optional<String> locale = ts.getLocale();
+        String localeSwe = "sv_SE";
+        assertNotEquals(locale, localeSwe);
+        assertNotEquals(null, localeSwe);
     }
 }
